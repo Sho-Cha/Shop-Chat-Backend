070738861aab85bbeeb5066b5db71a295a67f140
maheshpallapothula <maheshpallapothula@gmail.com>
2024-05-21 08:57:33 +0530

    Type ORM Module Set-Up

--- src/dataSource/dataSource.ts
+++ src/dataSource/dataSource.ts
@@ -1,18 +1,14 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import { User } from "../users/entities/user.entity";
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource } from "typeorm";
 
-export const dataSourceOptions: DataSourceOptions = {
-    type:"postgres",
-    host:process.env.HOST,
-    port:parseInt(process.env.PORT, 10),
-    username:"postgres",
-    password:"1234",
-    database:process.env.DB_DATABASE,
-    logging:false,
-    synchronize : false,
-    entities :['dist/**/*.entity.js'],
-    migrations:['dist/migrations/*.js']
-};
-const dataSource = new DataSource(dataSourceOptions);
-export default dataSource;
+export const dataBaseSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "1234",
+    database: "postgres",
+    logging: false,
+    synchronize: false,
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/migrations/*.js']
+})
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ConfigService } from './common/config/config.service';
 async function bootstrap() {
-  dotenv.config();
-  const BASE_PORT = ConfigService.get('PORT') ? ConfigService.get('PORT') : 3000;
+  const BASE_PORT = process.env.PORT ? process.env.PORT : 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(BASE_PORT);
-  console.warn(`Nest Application started on port :: ${BASE_PORT}`);
+  console.log(`Nest Application started on port :: ${BASE_PORT}`);
 }
 bootstrap();
