0617a05a3c9e468305e0f47f1fe8b33513c8b932
mahesh pallapothula <108536569+Maheshpallapothula@users.noreply.github.com>
2024-05-21 07:31:35 +0530

    Port file changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as dotenv from 'dotenv';
-
+import { ConfigService } from './common/config/config.service';
 async function bootstrap() {
   dotenv.config();
-  const BASE_PORT = process.env.PORT ? process.env.port : 3000;
+  const BASE_PORT = ConfigService.get('PORT') ? ConfigService.get('PORT') : 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(BASE_PORT);
   console.warn(`Nest Application started on port :: ${BASE_PORT}`);
